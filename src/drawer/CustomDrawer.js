46e542f1c3d3e60f9a9fb2435fddc53ac69a5c0e
pqthinh <phamquangquang2008@gmail.com>
2021-05-08 00:55:40 +0700

    add custom drawer

---
+++ src/drawer/CustomDrawer.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Linking,
+} from "react-native";
+import { styles } from "./styles";
+
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { Avatar } from "react-native-paper";
+
+const CustomSidebarMenu = (props) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Avatar.Image size={24} source={require("../../assets/logo_lg.png")} />
+      </View>
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label="Giới thiệu"
+          onPress={() => Linking.openURL("https://github.com/pqthinh/app")}
+        />
+        <View style={styles.customItem}>
+          <Text
+            onPress={() => {
+              Linking.openURL("https://github.com/pqthinh/app");
+            }}
+          >
+            Đánh giá
+          </Text>
+          <Image
+            source={{
+              uri:
+                "https://raw.githubusercontent.com/AboutReact/sampleresource/master/star_filled.png",
+            }}
+            style={styles.iconStyle}
+          />
+        </View>
+      </DrawerContentScrollView>
+      <View style={styles.footerDrawer}>
+        <Image
+          source={require("../../assets/logo_lg.png")}
+          style={styles.sideMenuProfileIcon}
+        />
+        <Text style={{ fontSize: 16, textAlign: "center", color: "grey" }}>
+          https://github.com/pqthinh/app
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default CustomSidebarMenu;
