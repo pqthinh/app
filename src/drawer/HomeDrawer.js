fc05bbce12b01a99bf65b96841dd1bb1d7ff57c1
pqthinh <phamquangquang2008@gmail.com>
2021-06-05 00:52:52 +0700

    fix route in drawer

--- src/drawer/HomeDrawer.js
+++ src/drawer/HomeDrawer.js
@@ -2,6 +2,8 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import DetailNews from "../feature/news/screen/detailsNews";
+import StackNotify from "../feature/Notification/StackNotify";
+import StackUser from "../feature/user/StackUser";
 import RootApp from "../navigation/RootStack";
 import CustomSidebarMenu from "./CustomDrawer";
 
@@ -14,8 +16,8 @@ const HomeDrawer = (props) => {
         drawerContent={(props) => <CustomSidebarMenu {...props} />}
       >
         <Drawer.Screen icon="home" name="Trang chủ" component={RootApp} />
-        <Drawer.Screen name="Thông báo" component={DetailNews} />
-        <Drawer.Screen name="Trang cá nhân" component={RootApp} />
+        <Drawer.Screen name="Thông báo" component={StackNotify} />
+        <Drawer.Screen name="Trang cá nhân" component={StackUser} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
