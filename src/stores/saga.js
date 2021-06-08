157a1619597e3d3452d0000ff6d0b296926477fb
pqthinh <phamquangquang2008@gmail.com>
2021-06-09 04:56:05 +0700

    mute saga home

--- src/stores/saga.js
+++ src/stores/saga.js
@@ -1,8 +1,8 @@
 import { all } from "redux-saga/effects";
 import { authSagas } from "../feature/authentication/saga";
-import { newsSagas } from "../feature/home/saga";
+// import { newsSagas } from "../feature/home/saga";
 
 export default function* rootSaga() {
   yield all([...authSagas]);
-  yield all([...newsSagas]);
+  // yield all([...newsSagas]);
 }
